abf57c839d2527bec25a14b8bfee6071e5ff4689
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-09-10 11:29:20 +0530

    import java.util.Scanner; class Season {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         if(a>=4 && a<=6)             System.out.println("Summer");         else if(a>=7 && a<=10)             System.out.println("Rainy");         else if(a==1 || a==11 ||a==12)             System.out.println("Winter");         else if(a==2 || a==3)             System.out.println("Spring");         else             System.out.println("-1");         sc.close();     } }

---
+++ Display_Season.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Season
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>=4 && a<=6)
+            System.out.println("Summer");
+        else if(a>=7 && a<=10)
+            System.out.println("Rainy");
+        else if(a==1 || a==11 ||a==12)
+            System.out.println("Winter");
+        else if(a==2 || a==3)
+            System.out.println("Spring");
+        else
+            System.out.println("-1");
+        sc.close();
+    }
+}
