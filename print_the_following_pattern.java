8018831a943e0eab959f861037dc8447d70b62ce
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-09-20 22:27:18 +0530

    import java.util.*; class Alpha_pattern {     public static void main(String args[])     {         Scanner sc=new Scanner (System.in);         int n=sc.nextInt();         for(int i=n;i>0;i--)         {             for(int j=1;j<=i;j++)             {                 System.out.print((char)(64+i)+" ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
-class nm_pattern
+import java.util.*;
+class Alpha_pattern
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner (System.in);
         int n=sc.nextInt();
-        for(int i=1;i<=n;i++)
+        for(int i=n;i>0;i--)
         {
             for(int j=1;j<=i;j++)
             {
-                System.out.print(j);
+                System.out.print((char)(64+i)+" ");
             }
             System.out.println();
         }
