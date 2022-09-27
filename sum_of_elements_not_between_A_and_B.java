017f00efe742ae136ef9eb8e76a5be155967d016
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-09-27 21:41:05 +0530

    import java.util.Scanner; class Sum_not_btw {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)             arr[i]=sc.nextInt();         int a=sc.nextInt();         int b=sc.nextInt();         int s=0;         for(int i=0;i<n;i++)             if(arr[i]<a || arr[i]>b)                 s+=arr[i];         System.out.print(s);     } }

---
+++ sum_of_elements_not_between_A_and_B.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Sum_not_btw
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int s=0;
+        for(int i=0;i<n;i++)
+            if(arr[i]<a || arr[i]>b)
+                s+=arr[i];
+        System.out.print(s);   
+    }
+}
