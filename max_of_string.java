47ae91b7f3f54f59cda2993b88b715c120a38814
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-10-01 13:46:58 +0530

    import java.util.Scanner; class Maxof_String {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int max=0;         for(int i=0;i<s.length();i++)         {             if((int)s.charAt(i)>max)                 max=s.charAt(i);         }         System.out.print((char)max);     } }

---
+++ max_of_string.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Maxof_String
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int max=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if((int)s.charAt(i)>max)
+                max=s.charAt(i);
+        }
+        System.out.print((char)max);
+    }
+}
