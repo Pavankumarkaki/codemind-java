f1a17b0c29d342e560ca9e09d8cade7bef0ff30a
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-10-03 18:54:17 +0530

    import java.util.Scanner; class Reverse_string {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         String st[]=s.split(" ");         //System.out.print(s);         for(int i=st.length-1;i>-1;i--)             System.out.print(st[i]+" ");     } }

--- Reverse_String.java
+++ Reverse_String.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
-class Reverse
+class Reverse_string
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
-        for(int i=s.length()-1;i>-1;i--)
-            System.out.print(s.charAt(i)+"");
+        String st[]=s.split(" ");
+        //System.out.print(s);
+        for(int i=st.length-1;i>-1;i--)
+            System.out.print(st[i]+" ");
     }
 }
